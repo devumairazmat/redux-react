4f51390cad91ccf21584e9784f80f45419766328
Umair Azmat <umairazmat8260@gmail.com>
2022-05-15 15:58:06 +0500

    Cart Card Added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import phone from "../../assets/images/mobile.png";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            {" "}
+            <span style={{ fontSize: "3em", color: "black", float: "right" }}>
+              <i class="fa-solid fa-cart-shopping"></i>
+            </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="card shadow-lg" style={{ width: "18rem" }}>
+            <img
+              className="card-img-top p-4"
+              src={phone}
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Mobile Phone</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+
+            <div className="card-body">
+              <button type="button" className="btn btn-primary">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
