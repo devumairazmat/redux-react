8e416429fc0d12834a1d54eb34fc9f63410387e8
Umair Azmat <umairazmat8260@gmail.com>
2022-05-15 15:06:54 +0500

    Navbar & Nav Container Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import HomeContainer from "./container/HomeContainer";
+import NavbarContainer from "./container/NavbarContainer";
 function App() {
   return (
     <div className="App">
       <h2>Redux Practice - Cart</h2>
       <HomeContainer />
+      <NavbarContainer />
     </div>
   );
 }
