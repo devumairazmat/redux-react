82d8398b02c1669cb7729cac28387d0c635e19a2
Umair Azmat <umairazmat8260@gmail.com>
2022-05-15 15:02:45 +0500

    Home Container Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import HomeContainer from "./container/HomeContainer";
 function App() {
   return (
     <div className="App">
       <h2>Redux Practice - Cart</h2>
+      <HomeContainer />
     </div>
   );
 }
